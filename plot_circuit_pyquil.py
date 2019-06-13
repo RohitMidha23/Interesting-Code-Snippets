eaa1e5f55156b9616a16863eac486ca4c1070170
RohitMidha23 <38888530+RohitMidha23@users.noreply.github.com>
2019-06-13 18:12:02 +0530

    Plot Circuit for Forest SDK

---
+++ plot_circuit_pyquil.py
@@ -0,0 +1,26 @@
+from pyquil import Program
+from pyquil.gates import H
+from pyquil.latex import to_latex
+import matplotlib.pyplot as plt
+import numpy as np
+import shutil
+import subprocess
+from tempfile import mkdtemp
+
+
+def plot_circuit(circuit):
+    latex_diagram = to_latex(circuit)
+    tmp_folder = mkdtemp()
+    with open(tmp_folder + '/circuit.tex', 'w') as f:
+        f.write(latex_diagram)
+    proc = subprocess.Popen(['pdflatex', '-shell-escape', tmp_folder + '/circuit.tex'], cwd=tmp_folder)
+    proc.communicate()
+    image = plt.imread(tmp_folder + '/circuit.png')
+    shutil.rmtree(tmp_folder)
+    plt.axis('off')
+    return plt.imshow(image)
+    
+circuit = Program()
+circuit += H(0)
+
+plot_circuit(circuit)
