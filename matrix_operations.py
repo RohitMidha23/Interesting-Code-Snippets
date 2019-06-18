26dc682530d34cc938f6209e6cebddca1be3e48f
unknown <rohit.midha23@gmail.com>
2019-06-18 15:45:48 +0530

    added : matrix minors

--- matrix_operations.py
+++ matrix_operations.py
@@ -9,3 +9,20 @@ def maxmin(m):
   b[0] = max(a[:,0])
   b[1] = min(a[:,1])
   return b
+
+def getMatrixMinor(m,r,c):
+  a = np.zeros([len(m)-1,len(m)-1])
+  p=0;q=0;
+  for i in range(len(m)):
+    for j in range(len(m)):
+      if r!=i and c!=j :
+        a[p,q] = m[i,j]
+        if j!=c :
+          q = q+1
+        if len(m)-1 == q :
+          q=0
+    if i!=r: p=p+1
+    if len(m)-1 == p:
+      p=p-1
+
+  return a
